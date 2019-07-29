271bce6a70a0384187f824462974c41318c126f1
Emily Dittmer <emilyedittmer@gmail.com>
2019-07-28 22:11:10 -0600

    Initial styling on card and card container

--- src/containers/SuggestionCard/SuggestionCard.jsx
+++ src/containers/SuggestionCard/SuggestionCard.jsx
@@ -1,43 +1,36 @@
-import React, { Component } from 'react';
-import './SuggestionCard.scss';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import "./SuggestionCard.scss";
+import { connect } from "react-redux";
 
 class SuggestionCard extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-
-    }
+    this.state = {};
   }
-
 
   render() {
     const { id, name, backgroundImg, voteAverage, overview, date } = this.props;
-    return(
-      <article
-      className="suggestion-card"
-      >
-      <div className="image--container">
-        <img src={backgroundImg} alt= "{name} poster"/>}
-      </div>
-      <div className="content">
-        <h3>{name}</h3>
-        <p>{voteAverage}/10</p>
-        <p>First Aired: {date}</p>
+    return (
+      <article className="suggestion-card">
+        <div className="image--container">
+          <img src={backgroundImg} alt="{name} poster" />
+        </div>
+        <div className="content">
+          <h3>{name}</h3>
+          <p>{voteAverage}/10</p>
+          <p>First Aired: {date}</p>
           <span className="sub-content">
             <div className="sub-border" />
             <p>{overview}</p>
           </span>
-      </div>
+        </div>
       </article>
-    )
+    );
   }
 }
 
 const mapStateToProps = state => ({
   suggestions: state.suggestions
-})
+});
 
-
-
-export default connect (mapStateToProps)(SuggestionCard);
+export default connect(mapStateToProps)(SuggestionCard);
